43ca6ec3336a45161888ac6eaa95a4625ccbed72
Srikanth Pavuluri <srikanthpavuluri99@gmail.com>
2021-12-21 23:08:53 +0530

    Update index.js

--- src/pages/ProductDetails/index.js
+++ src/pages/ProductDetails/index.js
@@ -1,27 +1,35 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import mockProducts from '../Products/mockdata';
 
 const ProductDetail = () => {
   const params = useParams();
+  const [product, setProduct] = useState({});
+ 
 
-  // map 
-  // filter or find
-  // filter() -> array -> return all the matching objects in array
-  // find() - > single object 
-  // console.log(params.id);
-  // find the product and show the product details
+useEffect(() => {
+  // make api call
+  fetch(`https://fakestoreapi.com/products/${params.id}`)
+      .then(res => {
+          console.log(res, 'res');
+          return res.json()
+      })
+      .then(json=>{
+          console.log(json)
+          setProduct(json)
+      }) 
+  }, []);
 
-// data type - number, string, object 
-// const product = mockProducts.filter(item => item.id == params.id);
+  console.log(params, 'productID');
 
-  const product = mockProducts.find(item => item.id == params.id);
-  
   return (
     <section>
       <h1>Product Detail</h1>
       <p>Id: {product.id}</p>
-      <p>Name: {product.name}</p>
+      <p>Name: {product.title}</p>
       <p>Description: {product.description}</p>
+      <img src={product.image} style={{width:'300px', height:'300px'}} />
+      <p>Rating: {product.rating?.rate} ({product.rating?.count}) </p>
     </section>
   );
 };
