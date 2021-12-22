25b3721e217de93836ea8a06796efa889bdf2cae
Srikanth Pavuluri <srikanthpavuluri99@gmail.com>
2021-12-22 23:42:23 +0530

    Update index.js

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Counter from '../../Counter';
+import InputField  from '../../components/Input';
 
 class ContactPerson extends Component { // Inheritance
     constructor (props) {
@@ -8,7 +8,8 @@ class ContactPerson extends Component { // Inheritance
             firstName: '', // this value
             lastName: '',
             email :'',
-            disableFirstName: false
+            mobile: '',
+            reason: ''
         };
         // useState, useEffect
     }
@@ -38,37 +39,86 @@ class ContactPerson extends Component { // Inheritance
     componentWillUnmount() {
         console.log("componentWillUnmount triggered"); // last
     }
-
+    // phases of life cycle
     // Mounting - Installing
     // Updating - 
     // UnMounting - Destroying
 
     // setState => to update the component state value
+    onChangeHandler = (name, value) => {
+        this.setState({
+            [name]: value
+        });
+    };
+
+    onSubmit = () => {
+        const {firstName, lastName, email, mobile, reason} = this.state;
+        const payload = {
+            'firstName': firstName,
+            'lastName': lastName,
+            'email': email,
+            mobile,
+            reason
+        };
+        alert(JSON.stringify(payload) + "Data submitted successfully");
+    };
+
+    onClear = () => {
+        this.setState({
+            firstName: '', // this value
+            lastName: '',
+            email :'',
+            mobile: '',
+            reason: ''
+        })
+    };
 
     render () {
-        const {firstName, lastName, email, disableFirstName} = this.state;
+        const {firstName, lastName, email, mobile, reason} = this.state;
         console.log("render triggered");
-        console.log({firstName, lastName, email}, "state values");
+        console.log(this.state, "state values");
         return (
             <div>
                 <p>FirstName: {firstName}</p>
-                <p><input type="text" disabled={disableFirstName} name="firstName" value={firstName} onChange={
-                    (event) =>  {
-                        console.log(event, 'event');
-                        this.setState({
-                            firstName: event.target.value
-                        })
-                    }}/></p>
+                <p>
+                    <InputField 
+                        fieldName={'firstName'} 
+                        fieldValue={firstName} 
+                        onChange={(name, value) => this.onChangeHandler(name, value)} />
+                    </p>
                 <p>LastName: {lastName}</p>
+                <p>
+                    <InputField 
+                        fieldName={'lastName'} 
+                        fieldValue={lastName} 
+                        onChange={(name, value) => this.onChangeHandler(name, value)} />
+                </p>
                 <p>Email: {email}</p>
-                <Counter />
+                <p>
+                    <InputField 
+                        fieldName={'email'} 
+                        fieldValue={email} 
+                        onChange={(name, value) => this.onChangeHandler(name, value)} />
+                </p>
+                <p>Mobile: {mobile}</p>
+                <p>
+                    <InputField 
+                        fieldName={'mobile'} 
+                        fieldValue={mobile} 
+                        onChange={(name, value) => this.onChangeHandler(name, value)} />
+                </p>
+                <p>
+                    Reason: {reason}
+                    <textarea 
+                        onChange={(event) => this.onChangeHandler('reason', event.target.value)} 
+                        value={reason} />
+                </p>
+
+                <button type='button' value="Clear" onClick={this.onClear} style={{margin: '10px'}} > Clear </button>
+                <button type='submit' value="Save" onClick={this.onSubmit} style={{margin: '10px'}} > Save </button>
             </div>
         );
     }
 };
 
 export default ContactPerson;
-
-
-
-
