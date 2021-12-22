831f7e8df1ec28a2adb1525fd451aa33834ad8c2
Srikanth Pavuluri <srikanthpavuluri99@gmail.com>
2021-12-22 23:41:47 +0530

    Update index.js

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -6,27 +6,34 @@ const MainHeader = () => {
   return (
     <header className={classes.header}>
       <nav>
-        <ul>
-          <li>
+        <ul style={{display: 'flex', justifyContent: 'space-around'}}>
+          <li style={{listStyle: 'none'}}>
             <NavLink activeClassName={classes.active} to='/'>
               Home Page
             </NavLink>
           </li>
-          <li>
+          <li style={{listStyle: 'none'}}>
             <NavLink activeClassName={classes.active} to='/products'>
               Products Page
             </NavLink>
           </li>
-          <li>
+          <li style={{listStyle: 'none'}}>
             <NavLink activeClassName={classes.active} to='/Users'>
               Users
             </NavLink>
           </li>
-          <li>
+          <li style={{listStyle: 'none'}}>
             <NavLink activeClassName={classes.active} to='/Counter'>
               Counter
             </NavLink>
           </li>
+
+          <li style={{listStyle: 'none'}}>
+            <NavLink activeClassName={classes.active} to='/Contact-Us'>
+              Contact Us
+            </NavLink>
+          </li>
+          
         </ul>
       </nav>
     </header>
